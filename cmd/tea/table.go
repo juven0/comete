a0b1850df8cfabea8f32bc8a08d5095199d95f4f
juvenO <bocquelin@gmail.com>
2024-02-21 22:11:41 +0300

    ajout de la table

--- cmd/tea/table.go
+++ cmd/tea/table.go
@@ -1,1 +1,34 @@
 package tea
+
+import (
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var basStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.BlockBorder()).
+	BorderBottomForeground(lipgloss.Color("240"))
+
+type model struct {
+	table table.Model
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return basStyle.Render(m.table.View()) + "\n"
+}
